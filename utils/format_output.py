a7b469b11f83a90dfc08e73646c833146ac8e5a3
Lawytel <adityadhiva88@gmail.com>
2023-06-04 17:24:49 +0700

    Modify api response format

--- utils/format_output.py
+++ utils/format_output.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class ImagePred(BaseModel):
     classes: list[str]
     prob: list[float]
-    top_2: list[dict]
+    top_2: dict
 
 # Format model prediction to JSON
 # for easy-to-read api response.
@@ -14,12 +14,13 @@ def format_image_pred(pred, classes):
     output = {}
     output['classes'] = classes
     output['prob'] = pred[0].tolist()
-    output['top_2'] = []
+    output['top_2'] = {}
     top_2_ind = pred[0].argsort()[::-1][:2]
     top_2_prob = pred[0][top_2_ind]
     top_2_classes = np.array(classes)[top_2_ind]
     for index, key in enumerate(top_2_classes):
-        output["top_2"].append({key: top_2_prob[index].item()})
+        # output["top_2"].append({key: top_2_prob[index].item()})
+        output["top_2"][key] = top_2_prob[index].item()
     output = ImagePred(classes=output['classes'], prob=output['prob'], top_2=output["top_2"])
     print(output)
     return output
